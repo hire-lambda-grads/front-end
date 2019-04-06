aaad97ad5d0e5c768affa53911e698ef31a596d7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-05 22:07:02 -0400

    Imports the following components into App.js: <About />, <Dashboard />, <Landing />, <Login />, <Map />, and <Signup />

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
+import About from './components/about/About';
+import Dashboard from './components/dashboard/Dashboard';
+import Landing from './components/landing/Landing';
+import Login from './components/login/Login';
+import Map from './components/map/Map';
+import Signup from './components/signup/Signup';
+
 
 class App extends Component {
   render() {
