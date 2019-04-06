d397c6529047bd2d87a77ac7b853761e83793988
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-06 00:20:17 -0400

    Adds a 4k video of whiteboarding as the background of the third section in the <Home />

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactMap from '../reactMap/ReactMap';
+import Travaho from '../../assets/Travaho.mp4';
+import Whiteboard from '../../assets/Whiteboard.mp4';
 
 const Home = () => {
     return(
         <div>
             <h1>Home/Landing Page</h1>
-            <h2 className="bg-video">Super Cool 4K Video background Here</h2>
+            <video className="bg-video" autoPlay loop muted>
+                <source src={Travaho} type='video/mp4' />
+            </video>
             <ReactMap />
-            <h2 className="bg-video">Another Super Cool 4K Video background Here</h2>
+            <video className="bg-video" autoPlay loop muted>
+                <source src={Whiteboard} type='video/mp4' />
+            </video>
             <h3 className="footer">Footer Goes Here</h3>
         </div>
     );
