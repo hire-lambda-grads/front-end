adffd4cea590e2c31aaa6410bad224f423fd0b0c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-05 22:20:35 -0400

    Creates <NavLinks> and <Routes> for all components except <Map />, which will be a subcomponent of <Landing />

--- src/App.js
+++ src/App.js
@@ -15,7 +15,59 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header>
+          <nav>
+            <NavLink exact to="/">Landing</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/about">About</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/dashboard">Dashboard</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/login">Login</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/signup">Signup</NavLink>
+            &nbsp;|&nbsp;
+            <button onClick={this.logout}>Logout</button>
+          </nav>
+        </header>
+        <main>
+        <Route
+            exact
+            path="/"
+            render={props => {
+              console.log(props);
+              return <Landing {...props} />;
+            }}
+          />
+          <Route
+            path="/about"
+            render={props => {
+              console.log(props);
+              return <About {...props} />;
+            }}
+          />
+          <Route
+            path="/dashboard"
+            render={props => {
+              console.log(props);
+              return <Dashboard {...props} />;
+            }}
+          />
+          <Route
+            path="/login"
+            render={props => {
+              console.log(props);
+              return <Login {...props} />;
+            }}
+          /> 
+          <Route
+            path="/signup"
+            render={props => {
+              console.log(props);
+              return <Signup {...props} />;
+            }}
+          />
+        </main>
       </div>
     );
   }
