f24926e2191aea1fd81434f6232ba2c9facb31c4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-05 22:37:28 -0400

    Builds out <Login />

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
-const Login = () => {
-    return(
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
         <div>
-            <h1>Login Page</h1>
-        </div>
+            <label htmlFor="username">
+                Username: 
+                <input
+                name="username"
+                id="username"
+                value={this.state.username}
+                onChange={this.handleInputChange}
+                type="text"
+                />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+                Password: 
+                <input
+                name="password"
+                id="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                type="password"
+                />
+            </label>
+          </div>
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
     );
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const endpoint = 'http://localhost:3300/api/login';
+
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        localStorage.setItem("token", res.data.token);
+
+        this.props.history.push('/dashboard');
+      })
+      .catch(error => console.error(error));
+  };
 }
 
 export default Login;
