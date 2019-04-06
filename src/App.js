006f95fb82e9f06f35715cc8ac7556d1a58d285f
Julian Moreno <julmo611@gmail.com>
2019-04-06 00:32:15 -0700

    Logo and Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.scss';
+import logo from './assets/hire-lambda-logo.svg';
 
 import About from './components/about/About';
 import Dashboard from './components/dashboard/Dashboard';
@@ -8,73 +9,79 @@ import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Signup from './components/signup/Signup';
 
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <nav>
-            <NavLink exact to="/">Home</NavLink>
-            &nbsp;|&nbsp;
-            <NavLink to="/about">About</NavLink>
-            &nbsp;|&nbsp;
-            <NavLink to="/dashboard">Dashboard</NavLink>
-            &nbsp;|&nbsp;
-            <NavLink to="/login">Login</NavLink>
-            &nbsp;|&nbsp;
-            <NavLink to="/signup">Signup</NavLink>
-            &nbsp;|&nbsp;
-            <button onClick={this.logout}>Logout</button>
-          </nav>
-        </header>
-        <main>
-        <Route
-            exact
-            path="/"
-            render={props => {
-              console.log(props);
-              return <Home {...props} />;
-            }}
-          />
-          <Route
-            path="/about"
-            render={props => {
-              console.log(props);
-              return <About {...props} />;
-            }}
-          />
-          <Route
-            path="/dashboard"
-            render={props => {
-              console.log(props);
-              return <Dashboard {...props} />;
-            }}
-          />
-          <Route
-            path="/login"
-            render={props => {
-              console.log(props);
-              return <Login {...props} />;
-            }}
-          /> 
-          <Route
-            path="/signup"
-            render={props => {
-              console.log(props);
-              return <Signup {...props} />;
-            }}
-          />
-        </main>
-      </div>
-    );
-  }
-  
-  logout = event => {
-    localStorage.removeItem("token");
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<div className="nav-bar">
+						<div className="hl-logo">
+							<img src={logo} alt="Logo" />
+						</div>
+						<nav>
+							<NavLink exact to="/">
+								Home
+							</NavLink>
+							&nbsp;|&nbsp;
+							<NavLink to="/about">About</NavLink>
+							&nbsp;|&nbsp;
+							<NavLink to="/dashboard">Dashboard</NavLink>
+							&nbsp;|&nbsp;
+							<NavLink to="/login">Login</NavLink>
+							&nbsp;|&nbsp;
+							<NavLink to="/signup">Signup</NavLink>
+							&nbsp;|&nbsp;
+							<button onClick={this.logout}>Logout</button>
+						</nav>
+					</div>
+				</header>
+				<main>
+					<Route
+						exact
+						path="/"
+						render={props => {
+							console.log(props);
+							return <Home {...props} />;
+						}}
+					/>
+					<Route
+						path="/about"
+						render={props => {
+							console.log(props);
+							return <About {...props} />;
+						}}
+					/>
+					<Route
+						path="/dashboard"
+						render={props => {
+							console.log(props);
+							return <Dashboard {...props} />;
+						}}
+					/>
+					<Route
+						path="/login"
+						render={props => {
+							console.log(props);
+							return <Login {...props} />;
+						}}
+					/>
+					<Route
+						path="/signup"
+						render={props => {
+							console.log(props);
+							return <Signup {...props} />;
+						}}
+					/>
+				</main>
+			</div>
+		);
+	}
 
-    this.props.history.push("/login");
-  };
+	logout = event => {
+		localStorage.removeItem('token');
+
+		this.props.history.push('/login');
+	};
 }
 
 export default withRouter(App);
