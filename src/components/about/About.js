6ba5f0b38a9e76cfea2df8c3ccf14c2da183757a
Jake Thomas <jmaxt12@gmail.com>
2019-04-07 17:58:11 -0500

    Some styling & vids working. Needs more styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,33 +3,39 @@ import React from 'react';
 
 import Card from './Card';
 
+
 //Videos for each person
-import jake from '../../assets/movie 1.mp4'
+import jake from '../../assets/Jake.mp4'
 
 
 const About = (props) => {
 	return (
-		<div>
+		<div className = 'about-cards'>
 			<h1>FOOTER MENU - Interactive About Us Page Goes Here</h1>
 			<h2>Social Cards for Each Team Member Appear Here</h2>
 			<Card 
 			name= 'Brandon Gardner'
+			video= {jake}
 			about= 'I am basically a walking computer! Calculating.....'
 			/>
 			<Card 
 			name= 'Tico Thepsourinthone'
+			video= {jake}
 			about= 'I am basically a walking computer! Calculating.....'
 			/>
 			<Card 
 			name= 'A. Lowell Richardson'
+			video= {jake}
 			about= 'I am basically a walking computer! Calculating.....'
 			/>
 			<Card 
 			name= 'Julian Moreno'
+			video= {jake}
 			about= 'I am basically a walking computer! Calculating.....'
 			/>
 			<Card 
 			name= 'Ryan Clark'
+			video= {jake}
 			about= 'I am basically a walking computer! Calculating.....'
 			/>
 			<Card 
@@ -37,10 +43,7 @@ const About = (props) => {
 			video= {jake}
 			about= 'I am basically a walking computer! Calculating.....'
 			/>
-			<Card 
-			name= 'Jamie Goodnight'
-			about= 'I am basically a walking computer! Calculating.....'
-			/>
+			
 		</div>
 	);
 	
--- src/components/about/Card.js
+++ src/components/about/Card.js
@@ -1,36 +1,41 @@
 import React from 'react';
 
-import jake from '../../assets/movie 1.mp4'
 
 class Card extends React.Component {
-  playVideo() {
-    this.refs.vidRef.play();
-  }
-  
-  pauseVideo() {
-    this.refs.vidRef.pause();
-  }
-    
-    render(props) {
-      return(
-        <div className = 'about-card'>
-            <h2>{this.props.name}</h2>
-          <video 
-          height='200'
-          width='200'
-          loop
-          ref="vidRef" 
-          src={this.props.video} 
-          type="video/mp4"
-          onMouseOver={this.playVideo.bind(this)} 
-          onMouseOut={this.pauseVideo.bind(this)}
-          ></video>
-          <p>{this.props.about}</p>
-        </div>
-      );
+    playVideo() {
+        this.refs.vidRef.play();
     }
-  }
-  
-  
-  
- export default Card
+
+    pauseVideo() {
+        this.refs.vidRef.pause();
+    }
+
+
+
+    render(props) {
+        return (
+            <div className='about-card'>
+                <h2>{this.props.name}</h2>
+                    <video
+                        muted
+                        loop
+                        ref="vidRef"
+                        src={this.props.video}
+                        type="video/mp4"
+                        onMouseOver={this.playVideo.bind(this)}
+                        onMouseOut={this.pauseVideo.bind(this)}>
+                    </video>
+                <div className='circle'>
+                </div>
+
+                <p>{this.props.about}</p>
+
+                <br />
+            </div>
+        );
+    }
+}
+
+
+
+export default Card
