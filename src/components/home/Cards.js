de8eb0e8cfd455e290e40ba266929ebe77c8eace
Julian Moreno <julmo611@gmail.com>
2019-04-10 09:57:31 -0700

    some css changes

--- src/components/home/Cards.js
+++ src/components/home/Cards.js
@@ -274,6 +274,36 @@ export default class Cards extends Component {
 					</div>
 				</div>
 				{/* end */}
+				<div className="card">
+					<div className="firstinfo">
+						<div className="name-box">
+							<div className="img-wrap">
+								<img src="" alt="Profile Pictuare" />
+							</div>
+							<h1>Full Name</h1>
+							<p>Full Stack Web Development and CS</p>
+							<p>Skills</p>
+							<p>Location</p>
+							<p>Relocation</p>
+						</div>
+					</div>
+				</div>
+				{/* end */}
+				<div className="card">
+					<div className="firstinfo">
+						<div className="name-box">
+							<div className="img-wrap">
+								<img src="" alt="Profile Pictuare" />
+							</div>
+							<h1>Full Name</h1>
+							<p>Full Stack Web Development and CS</p>
+							<p>Skills</p>
+							<p>Location</p>
+							<p>Relocation</p>
+						</div>
+					</div>
+				</div>
+				{/* end */}
 			</>
 		);
 	}
