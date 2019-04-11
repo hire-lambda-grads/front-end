f1a810c4cb79afb2fb5ee301a5d3cdeeeae2d18d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-11 00:07:20 -0400

    Updates mapStyle to 'dark-v9'

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from "react";
+import ReactMapGL from "react-map-gl";
 
-import ReactMapGL from 'react-map-gl';
-
-const MAPBOX_TOKEN = 'pk.eyJ1IjoidGljb3RoZXBzIiwiYSI6ImNqdTl4ZTRwYjBhdTY0M3FxZzhpY3FmZWcifQ.zt0JGIlN2B3nLi4d7yBaew';
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoidGljb3RoZXBzIiwiYSI6ImNqdTl4ZTRwYjBhdTY0M3FxZzhpY3FmZWcifQ.zt0JGIlN2B3nLi4d7yBaew";
 
 class Map extends React.Component {
-
   state = {
     viewport: {
       width: 1000,
       height: 650,
       latitude: 39.788260590328576,
       longitude: -97.77255674948162,
-      zoom: 4
+      zoom: 3.5
     }
+  };
+
+  _goToNYC = () => {
+    const viewport = {
+      ...this.state.viewport,
+      longitude: -74.1,
+      latitude: 40.7
+    };
+    this.setState({ viewport });
   };
 
   render() {
     return (
       <ReactMapGL
-        {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
+		{...this.state.viewport}
+		mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={viewport => this.setState({ viewport })}
         mapboxApiAccessToken={MAPBOX_TOKEN}
-      >
-      </ReactMapGL>
+      />
     );
   }
 }
