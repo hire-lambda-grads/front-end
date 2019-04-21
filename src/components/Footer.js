c3788ec0e1fefd20511d2d6ccce091838e810e84
Julian Moreno <julmo611@gmail.com>
2019-04-20 22:22:29 -0700

    New logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,52 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logoWhite from '../assets/hire-lambda-logo-white.svg';
+import logoWhite from '../assets/Hire-lambda-logo-white.png';
 
 const Footer = () => {
-  return ( 
-    <footer>
-      <nav>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
+	return (
+		<footer>
+			<nav>
+				<NavLink exact to="/">
+					Home
+				</NavLink>
 
-        <NavLink to="/about">About</NavLink>
+				<NavLink to="/about">About</NavLink>
 
-        <NavLink to="/privacy-policy">Privacy Policy</NavLink>
-        <NavLink to="/contact">Contact Hire Lambda</NavLink>
-      </nav>
-      <div className="hl-logo">
-        <NavLink exact to="/">
-          <img src={logoWhite} alt="Logo" />
-        </NavLink>
-      </div>
-      <div className="footer-icons">
-        <div className="icons-container">
-          <a
-            href="https://twitter.com/lambdaschool"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-twitter" />
-          </a>
-          <a
-            href="https://www.facebook.com/LambdaSchoolOnline/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-facebook-square" />
-          </a>
-          <a
-            href="https://github.com/LambdaSchool"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github" />
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
-}
- 
+				<NavLink to="/privacy-policy">Privacy Policy</NavLink>
+				<NavLink to="/contact">Contact Hire Lambda</NavLink>
+			</nav>
+			<div className="hl-logo">
+				<NavLink exact to="/">
+					<img src={logoWhite} alt="Logo" />
+				</NavLink>
+			</div>
+			<div className="footer-icons">
+				<div className="icons-container">
+					<a
+						href="https://twitter.com/lambdaschool"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="fab fa-twitter" />
+					</a>
+					<a
+						href="https://www.facebook.com/LambdaSchoolOnline/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="fab fa-facebook-square" />
+					</a>
+					<a
+						href="https://github.com/LambdaSchool"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="fab fa-github" />
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import logo from '../assets/hire-lambda-logo.svg';
-
+import logo from '../assets/Hire-lambda-logo.png';
 
 const Header = () => {
-  return ( 
-    <header>
-      <div className="top-bar">
-        <div className="hl-logo">
-          <NavLink exact to="/">
-            <img src={logo} alt="Logo" />
-          </NavLink>
-        </div>
-        <Link to="/login" className="btn-red">
-          login <i className="fas fa-sign-out-alt" />
-        </Link>
-      </div>
-    </header>
-  );
-}
- 
+	return (
+		<header>
+			<div className="top-bar">
+				<div className="hl-logo">
+					<NavLink exact to="/">
+						<img src={logo} alt="Logo" />
+					</NavLink>
+				</div>
+				<Link to="/login" className="btn-red">
+					login <i className="fas fa-sign-out-alt" />
+				</Link>
+			</div>
+		</header>
+	);
+};
+
 export default Header;
